677462f132fc2749fa5b02768eb420fbba9335de
AwesomeJim <jimnahgua@gmail.com>
2018-06-27 22:08:33 +0300

    Added Add task Activity and its layout

--- app/src/main/java/com/jim/quickjournal/AddJournalActivity.java
+++ app/src/main/java/com/jim/quickjournal/AddJournalActivity.java
@@ -9,5 +9,8 @@ public class AddJournalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_journal);
+            getSupportActionBar().setIcon(R.drawable.ic_edit_journal);
+            getSupportActionBar().setDisplayShowCustomEnabled(true);
+
     }
 }
--- app/src/main/java/com/jim/quickjournal/MainActivity.java
+++ app/src/main/java/com/jim/quickjournal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jim.quickjournal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,13 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().setIcon(R.drawable.ic_journal);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent startAddJournalActivity=new Intent(MainActivity.this, AddJournalActivity.class);
+                startActivity(startAddJournalActivity);
             }
         });
     }
