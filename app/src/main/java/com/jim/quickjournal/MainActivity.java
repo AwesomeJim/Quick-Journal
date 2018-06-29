3887d0172f8252b6924989438c9830c24bb0cf8d
awesomejim <jimnahgua@gmail.com>
2018-06-29 17:09:55 +0300

    Update to use Navigation Drawer

--- app/src/main/java/com/jim/quickjournal/MainActivity.java
+++ app/src/main/java/com/jim/quickjournal/MainActivity.java
@@ -3,14 +3,17 @@ package com.jim.quickjournal;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,9 +22,7 @@ import com.jim.quickjournal.data.JournalAdapter;
 import com.jim.quickjournal.data.JournalEntry;
 import com.jim.quickjournal.data.database.JournalDatabase;
 
-public class MainActivity extends AppCompatActivity implements JournalAdapter.ItemClickListener {
-
-
+public class MainActivity extends AppCompatActivity implements JournalAdapter.ItemClickListener,NavigationView.OnNavigationItemSelectedListener {
 
     // Constant for logging
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -38,14 +39,13 @@ public class MainActivity extends AppCompatActivity implements JournalAdapter.It
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        if(getSupportActionBar()!=null) {
-            getSupportActionBar().setIcon(R.drawable.ic_journal);
-        }
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         mDb=JournalDatabase.getInstance(getApplicationContext());
 
         // Set the RecyclerView to its corresponding view
         mRecyclerView = findViewById(R.id.recyclerView);
+
 
         // Set the layout for the RecyclerView to be a linear layout, which measures and
         // positions items within a RecyclerView into a linear list
@@ -64,7 +64,16 @@ public class MainActivity extends AppCompatActivity implements JournalAdapter.It
                 startActivity(startAddJournalActivity);
             }
         });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,16 +83,32 @@ public class MainActivity extends AppCompatActivity implements JournalAdapter.It
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -122,7 +147,7 @@ public class MainActivity extends AppCompatActivity implements JournalAdapter.It
                     }
                 })
                 .setIcon(R.drawable.ic_journal_entry)
-                .setNeutralButton("ok", null)
+                .setNeutralButton("Ok", null)
                 .show();
     }
 }
