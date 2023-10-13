520bddc28d378de9a10cfde080ec9b34ae5b6228
AwesomeJim <8529047+AwesomeJim@users.noreply.github.com>
2023-10-13 10:40:58 +0300

    Added App TopAppBar component

---
+++ app/src/main/java/com/jim/quickjournal/ui/components/Text.kt
@@ -0,0 +1,54 @@
+package com.jim.quickjournal.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Subtitle(text: String, modifier: Modifier = Modifier, color: Color = Color.Unspecified) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleLarge,
+        modifier = modifier.padding(horizontal = 8.dp, vertical = 2.dp),
+        color = color,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun SubtitleSmall(text: String, modifier: Modifier = Modifier, color: Color = Color.Unspecified) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleSmall,
+        modifier = modifier.padding(horizontal = 16.dp, vertical = 0.dp),
+        color = color
+    )
+}
+
+
+@Composable
+fun Headline(text: String, modifier: Modifier = Modifier, color: Color = Color.Unspecified) {
+    Text(
+        text = text,
+        modifier = modifier,
+        style = MaterialTheme.typography.displayMedium,
+        color = color
+    )
+}
+
+
+@Composable
+fun BodyText(text: String, modifier: Modifier = Modifier, color: Color = Color.Unspecified) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+        color = color,
+        modifier = modifier
+    )
+}
+
---
+++ app/src/main/java/com/jim/quickjournal/ui/components/TopAppBar.kt
@@ -0,0 +1,145 @@
+package com.jim.quickjournal.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.EditNote
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jim.quickjournal.R
+import com.jim.quickjournal.ui.theme.QuickJournalTheme
+
+/**
+ * Created by awesome jim on.
+ * 29/09/2023
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QuickJournalTopAppBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    navigateUp: () -> Unit = {},
+    canNavigateBack: Boolean = false,
+    editButtonState: Boolean = false,
+    onEditButtonClicked: () -> Unit = {},
+    cancelButtonState: Boolean = false,
+    onCancelButtonClicked: () -> Unit = {},
+    saveButtonState: Boolean = false,
+    onSaveButtonClicked: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                text = title,
+                fontSize = 22.sp,
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        ),
+        modifier = modifier,
+        actions = {
+            if (editButtonState) {
+                IconButton(onClick = onEditButtonClicked) {
+                    Icon(
+                        imageVector = Icons.Filled.EditNote,
+                        contentDescription = "Edit Journal"
+                    )
+                }
+            }
+            if (cancelButtonState) {
+                IconButton(onClick = onCancelButtonClicked) {
+                    Icon(
+                        imageVector = Icons.Filled.Cancel,
+                        contentDescription = "Cancel Editing Journal"
+                    )
+                }
+            }
+            if (saveButtonState) {
+                IconButton(onClick = onSaveButtonClicked) {
+                    Icon(
+                        imageVector = Icons.Filled.Save,
+                        contentDescription = "Save Journal"
+                    )
+                }
+            }
+        },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TopAppBarPreview() {
+    QuickJournalTheme {
+        QuickJournalTopAppBar("Awesome Jim")
+    }
+}
+
+
+/**
+ * Shows the floating action button.
+ *
+ * @param extended Whether the tab should be shown in its expanded state.
+ */
+@Composable
+fun AddFloatingActionButton(
+    extended: Boolean,
+    onClick: () -> Unit
+) {
+    // how it should animate.
+    FloatingActionButton(
+        onClick = onClick,
+        modifier = Modifier.padding(8.dp),
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add a journal",
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+            // Toggle the visibility of the content with animation.
+            AnimatedVisibility(extended) {
+                Text(
+                    text = "Add a Journal",
+                    modifier = Modifier
+                        .padding(start = 8.dp, top = 3.dp)
+                )
+            }
+        }
+    }
+}
